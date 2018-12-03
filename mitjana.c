2e5a5f41a0fff61230d3457b86122b67051a3f9a
nadia <nadieta@gmail.com>
2018-12-03 10:34:04 +0100

    Tercer commit del programa mitjana

--- mitjana.c
+++ mitjana.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-
+#include <stdio.h
 int main() {
-	float suma=0;
-	float mitjana;
+	float suma=0.0;
+	float mitjana=0.0;
 	float nou_valor;
 	int numero_repeticions=0;
 	char consulta='s';
